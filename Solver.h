b56ebae0d9c2306487c188fd8828d49012203976
leibori <ori.leibo@gmail.com>
2019-01-04 15:38:19 +0200

    first upload of files

---
+++ Solver.h
@@ -0,0 +1,11 @@
+//
+// Created by ori on 1/4/19.
+//
+
+#ifndef SOLIDPROJECT_EX2_SOLVER_H
+#define SOLIDPROJECT_EX2_SOLVER_H
+template <class Problem, class Solution>
+class Solver {
+    virtual Solution solve (Problem problem) = 0 ;
+};
+#endif //SOLIDPROJECT_EX2_SOLVER_H
