463552fbd99aa1c60ed65c6ef5b3160773db0344
amitAshke <amit471@gmail.com>
2019-01-14 23:52:22 +0200

    MyTestClientHandler was updated to use templates.

--- MyTestClientHandler.h
+++ MyTestClientHandler.h
@@ -2,6 +2,7 @@
 #define SOLIDPROJECT_EX2_MYTESTCLIENTHANDLER_H
 
 #include <iostream>
+#include <string>
 #include <string.h>
 #include <unistd.h>
 #include <vector>
@@ -11,16 +12,72 @@
 #include "CacheManager.h"
 #include "MyMatrix.h"
 
+using namespace std;
+
+template <class P, class S>
 class MyTestClientHandler : public ClientHandler {
-    Solver<MyMatrix, string> *solver;
-    CacheManager<MyMatrix, string> *cacheManager;
+    Convertor<P,S> *convertor;
+    Solver<P,S> *solver;
+    CacheManager<P,S> *cacheManager;
 public:
-    MyTestClientHandler(Solver<MyMatrix, string> *solver1, CacheManager<MyMatrix, string> *cacheManager1) {
+    MyTestClientHandler(Convertor<P,S> *convertor1, Solver<P,S> *solver1, CacheManager<P,S> *cacheManager1) {
+        convertor = convertor1;
         solver = solver1;
         cacheManager = cacheManager1;
     }
-    bool isNumber(string line);
-    void handleClient(int socket);
+    void handleClient(int socket)  {
+        S solution;
+        P convertedProblem;
+        string line, convertedSolution;
+        vector<string> problem;
+        char buffer[2];
+        bool isFirstLine, stop = false;
+        int dataLines, lineIndex;
+        while (true) {
+            dataLines = 3;
+            isFirstLine = true;
+            lineIndex = 0;
+            do {
+                bzero(buffer, 2);
+                line = "";
+                ssize_t bytes_read;
+                do {
+                    bytes_read = read(socket, buffer, 1);
+                    if (bytes_read < 0) {
+                        __throw_bad_exception();
+                    }
+                    line += buffer[0];
+                } while (buffer[0] != *"\n" && buffer[0] != *"\r\n");
+                if (line == "end\n" || line == "end\r\n") {
+                    stop = true;
+                    break;
+                } else if (isFirstLine) {
+                    isFirstLine = false;
+                    for (int i = 0; i < line.size(); i++) {
+                        if (line[i] == ',') {
+                            dataLines++;
+                        }
+                    }
+                    problem.clear();
+                }
+                problem.push_back(line);
+                lineIndex++;
+            } while (lineIndex != dataLines);
+            if (stop) {
+                break;
+            }
+            convertedProblem = convertor->conVecStringToProblem(problem);
+            if (cacheManager->isExistProb(convertedProblem)) {
+                solution = cacheManager->getSol(convertedProblem);
+            } else {
+                solution = solver->solve(problem);
+            }
+            convertedSolution = convertor->conSolvToString(solution);
+            if (write(socket, convertedSolution.c_str(), (int)(convertedSolution.length())) < 0) {
+                throw "error writing data to socket";
+            }
+        }
+    }
 };
 
 #endif //SOLIDPROJECT_EX2_MYTESTCLIENTHANDLER_H
